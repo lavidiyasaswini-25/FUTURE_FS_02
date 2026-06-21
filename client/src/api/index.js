6241aecf303fdff8c24bea2b6ae35d26a800e3f9
Yasaswini <lavidiyasaswini@gmail.com>
2026-06-21 19:50:46 +0530

    Connect frontend to live backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://future-fs-02-au3l.onrender.com/api' });
 
 // Attach JWT token to every request
-API.interceptors.request.use((req) => {
+API.interceptors.request.use((req) => 
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
